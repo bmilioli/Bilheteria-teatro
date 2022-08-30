12e36e93926a9464a778ab9534b98f9fa0d73361
bmilioli <brunomiliolif@gmail.com>
2022-08-30 09:37:20 -0300

    Final

--- TeatroIP.c
+++ TeatroIP.c
@@ -1,32 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// Bibliotecas usadas no programa.
+#include <stdio.h>  //Entrada e saída padrão.
+#include <stdlib.h> //Emula o prompt do SO correspondente.
+#include <string.h> //Manipular strings
 
 typedef struct
-{
+{ // Estrutura dos dados pessoais.
+
     char nome[40];
     char tel[15];
     char email[40];
-    char status[2];
+    char status;
     int tipo;
-} cadeiras;
+
+} CADEIRAS;
+
+typedef struct
+{ // Estrutura dos dados da peça.
+
+    CADEIRAS cadeira[22][30];
+    char dataPeca[11];
+    char nomePeca[60];
+    char horarioPeca[6];
+    float valorArrecadado;
+
+} TEATRO;
+
+typedef char RESERVAS[120];
+
+typedef struct
+{
+    RESERVAS rel[100];
+    int ult;
+
+} RELATO;
 
 void menu()
 { // Menu inicial do programa.
+
     printf("MENU PRINCIPAL \n\n");
     printf("1 - Visualizar mapa do teatro\n");
     printf("2 - Comprar cadeira\n");
     printf("3 - Reservar cadeira\n");
-    printf("4 - Consultar uma determinada cadeira da Sala\n");
-    printf("5 - Total arrecado\n");
-    printf("6 - Sair do Aplicativo\n");
+    printf("4 - Consultar uma determinada cadeira da sala\n");
+    printf("5 - Total arrecadado\n");
+    printf("6 - Cancelar todas as reservas\n");
+    printf("7 - Desocupar cadeira especifica\n");
+    printf("8 - Gerar relatório\n");
+    printf("9 - Sair do aplicativo\n");
     printf("Digite o numero de uma das opcoes acima: ");
 }
 
-void letranumero(int numero) // Transforma o número em uma letra de fileira.
-{
+void letranumero(int numero)
+{ // Transforma o numero em uma letra de fileira.
+
     switch (numero)
-    {
+    { // Análise do número para a troca em letra.
+
     case 0:
         printf("A");
         break;
@@ -108,10 +137,12 @@ void letranumero(int numero) // Transforma o número em uma letra de fileira.
     }
 }
 
-void numeroletra(char letra, int *n) // Transforma a letra de uma fileira digitada em um numero.
-{
+void numeroletra(char letra, int *n)
+{ // Transforma a letra de uma fileira digitada em um numero.
+
     switch (letra)
-    {
+    { // Análise da letra para a troca em número.
+
     case 'A':
         *n = 0;
         break;
@@ -193,62 +224,157 @@ void numeroletra(char letra, int *n) // Transforma a letra de uma fileira digita
     }
 }
 
-void zerar(cadeiras vetor[22][30])
-{
+void zerar(TEATRO *t)
+{ // Função para desocupar todo o teatro.
 
     int i, j;
-    for (i = 0; i < 22; i++)
+    for (i = 0; i < 22; i++) // Ciclo de varredura da matriz do teatro.
         for (j = 0; j < 30; j++)
-        {
-            strcpy(vetor[i][j].nome, " ");
-            strcpy(vetor[i][j].tel, " ");
-            strcpy(vetor[i][j].email, " ");
-            strcpy(vetor[i][j].status, "L");
-            vetor[i][j].tipo = 0;
-        }
+            t->cadeira[i][j].status = 'L';
 }
 
-void imprimirDados(int i, int j, cadeiras cadeira[22][30])
-{
-    printf("%s", cadeira[i][j].nome);
-    printf("Telefone: %s", cadeira[i][j].tel);
-    printf("Email: %s", cadeira[i][j].email);
-    if (cadeira[i][j].tipo == 1)
+void imprimirDados(int i, int j, TEATRO t)
+{ // Função para imprimir os dados do cliente e do tipo de entrada dele.
+
+    printf("%s", t.cadeira[i][j].nome);
+    printf("Telefone: %s", t.cadeira[i][j].tel);
+    printf("Email: %s", t.cadeira[i][j].email);
+
+    if (t.cadeira[i][j].tipo == 1)
     {
+
         printf("Meia entrada\n");
     }
-    else if (cadeira[i][j].tipo == 2)
+    else if (t.cadeira[i][j].tipo == 2)
     {
+
         printf("Entrada inteira\n");
     }
 }
 
-int main()
+void criar_arq(TEATRO t)
+{ // Função para criar o aquivo do programa - sem a struct.
+
+    FILE *fp;
+
+    fp = fopen("Teatro.dat", "wb"); // Arquivo para escrita binaria.
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+
+        return;
+    }
+    else
+    {
+        printf("Arquivo Binario criado com sucesso.\n");
+        fwrite(&t, sizeof(TEATRO), 1, fp);
+    }
+    fclose(fp);
+
+} // Função reponsavel por exibir o conteudo do arquivo Binario.dat
+
+void Carregar_Arq(TEATRO *t)
+{ // Função reponsavel por exibir o conteudo do arquivo Binario.dat
+
+    FILE *fp;
+
+    fp = fopen("Teatro.dat", "rb");
+    if (fp == NULL)
+    {
+
+        zerar(&(*t));
+        printf("Digite o nome do espetaculo: ");
+        fgets(t->nomePeca, 60, stdin);
+        printf("Digite a data do espetaculo: ");
+        fgets(t->dataPeca, 11, stdin);
+        printf("Digite o horario do espetaculo: ");
+        fgets(t->horarioPeca, 6, stdin);
+        printf("\n");
+
+        return;
+    }
+    else
+    {
+
+        fread(&(*t), sizeof(TEATRO), 1, fp);
+        fclose(fp);
+    }
+}
+
+void Imprimir_infosPeca(TEATRO *t)
+{ // Função para imprimir as informações inseridas sobre a peça.
+
+    printf("\n");
+    printf("O espetaculo sera: %s", t->nomePeca);
+    printf("Data do evento: %s", t->dataPeca);
+    printf("Horario do evento: %s\n", t->horarioPeca);
+    printf("\n");
+
+    return;
+}
+
+void relatorio(RELATO r)
 {
-    cadeiras cadeira[22][30];
-    int i, j, cont = 0, cont2 = 0;
+
+    FILE *pFile;
+    int i;
+
+    pFile = fopen("Reservas.txt", "wt");
+
+    if (pFile == NULL)
+    {
+        printf("Arquivo de saida nao foi criado - Terminando o programa.\n");
+        exit(1);
+    }
+    else
+    {
+
+        for (i = 0; i < r.ult; i++)
+            fprintf(pFile, "%s\n", r.rel[i]);
+
+        fclose(pFile);
+    }
+}
+
+int main()
+{ // Função principal.
+
+    RELATO r;
+    TEATRO t;
+    int i, j, k, cont = 0, contr = 0;
     int op;
     char aux;
-    zerar(cadeira);
+
+    r.ult = 0;
+
+    Carregar_Arq(&t); // Chamando da função para carregar o arquivo.
 
     printf("  ******* T E A T R O ******* \n");
-    do
+
+    Imprimir_infosPeca(&t); // Chamada da função que insere as informações sobre o evento.
+
+    do // Ciclo responsável pela operação do menu.
     {
         menu();
         scanf("%d", &op);
         printf("\n");
 
         switch (op)
-        {
-        case 1:
+        { // Análise do dígito de operação do menu.
+
+            // System("clear") funciona no Linux, no Windows usa-se "CLS".
+            // System("clear") usado apenas nas opções as quais não dependem do mapa do teatro para a experiência completa do programa.
+
+        case 1: // Primeira opção: Exibir planta do teatro.
+            system("clear");
+
             for (i = 0; i < 22; i++)
             {
                 for (j = 0; j < 15; j++)
                 {
                     letranumero(i);
                     printf("%d-", j + 1);
-
-                    printf("%s ", cadeira[i][j].status);
+                    printf("%c ", t.cadeira[i][j].status);
                 }
 
                 printf("       ");
@@ -257,14 +383,14 @@ int main()
                 {
                     letranumero(i);
                     printf("%d-", j + 1);
-                    printf("%s ", cadeira[i][j].status);
+                    printf("%c ", t.cadeira[i][j].status);
                 }
                 printf("\n");
             }
             break;
 
-        case 2:
-            // system("clear");
+        case 2: // Segunda opção, comprar cadeira.
+
             printf("Digite fileira da cadeira que deseja comprar: ");
             scanf(" %c", &aux);
             numeroletra(aux, &i);
@@ -272,36 +398,42 @@ int main()
             scanf("%d", &j);
             getchar();
             j--;
-            if (cadeira[i][j].status[0] == 'L')
-            {
-                printf("Digite o nome do cliente: ");
-                fgets(cadeira[i][j].nome, 40, stdin);
-                printf("Digite o telefone do cliente: ");
-                fgets(cadeira[i][j].tel, 15, stdin);
-                printf("Digite o email do cliente: ");
-                fgets(cadeira[i][j].email, 40, stdin);
-                printf("Digite 1 pra Meia e 2 para Inteira: ");
-                scanf("%d", &cadeira[i][j].tipo);
 
-                if (cadeira[i][j].tipo == 1)
-                {
+            if (t.cadeira[i][j].status == 'L')
+            {
+
+                printf("Digite o nome do cliente: ");
+                fgets(t.cadeira[i][j].nome, 40, stdin);
+                printf("Digite o telefone do cliente: ");
+                fgets(t.cadeira[i][j].tel, 15, stdin);
+                printf("Digite o email do cliente: ");
+                fgets(t.cadeira[i][j].email, 40, stdin);
+                printf("Digite 1 pra Meia e 2 para Inteira: ");
+                scanf("%d", &t.cadeira[i][j].tipo);
+
+                if (t.cadeira[i][j].tipo == 1)
+                { // Contador de entrada completa e meia entrada.
+
                     cont++;
                 }
                 else
                 {
-                    cont2++;
+
+                    cont += 2;
                 }
 
-                if (cadeira[i][j].tipo != 1 && cadeira[i][j].tipo != 2)
+                if (t.cadeira[i][j].tipo != 1 && t.cadeira[i][j].tipo != 2)
                 {
+
                     do
                     {
+
                         printf("Tipo invalido, digite 1 pra Meia e 2 para Inteira: ");
-                        scanf("%d", &cadeira[i][j].tipo);
-                    } while (cadeira[i][j].tipo != 1 && cadeira[i][j].tipo != 2);
+                        scanf("%d", &t.cadeira[i][j].tipo);
+                    } while (t.cadeira[i][j].tipo != 1 && t.cadeira[i][j].tipo != 2);
                 }
 
-                strcpy(cadeira[i][j].status, "O");
+                t.cadeira[i][j].status = 'O';
                 printf("\nCadeira comprada com sucesso!\n");
             }
             else
@@ -310,7 +442,8 @@ int main()
             }
             break;
 
-        case 3:
+        case 3: // Terceira opção, reservar cadeira.
+
             printf("Digite fileira da cadeira que deseja reservar: ");
             scanf(" %c", &aux);
             numeroletra(aux, &i);
@@ -318,36 +451,41 @@ int main()
             scanf("%d", &j);
             getchar();
             j--;
-            if (cadeira[i][j].status[0] == 'L')
-            {
-                printf("Digite o nome do cliente: ");
-                fgets(cadeira[i][j].nome, 40, stdin);
-                printf("Digite o telefone do cliente: ");
-                fgets(cadeira[i][j].tel, 15, stdin);
-                printf("Digite o email do cliente: ");
-                fgets(cadeira[i][j].email, 40, stdin);
-                printf("Digite 1 pra Meia e 2 para Inteira: ");
-                scanf("%d", &cadeira[i][j].tipo);
 
-                if (cadeira[i][j].tipo == 1)
-                {
-                    cont++;
+            if (t.cadeira[i][j].status == 'L')
+            {
+
+                printf("Digite o nome do cliente: ");
+                fgets(t.cadeira[i][j].nome, 40, stdin);
+                printf("Digite o telefone do cliente: ");
+                fgets(t.cadeira[i][j].tel, 15, stdin);
+                printf("Digite o email do cliente: ");
+                fgets(t.cadeira[i][j].email, 40, stdin);
+                printf("Digite 1 pra Meia e 2 para Inteira: ");
+                scanf("%d", &t.cadeira[i][j].tipo);
+
+                if (t.cadeira[i][j].tipo == 1)
+                { // Contador de entrada completa e meia entrada.
+
+                    contr++;
                 }
                 else
                 {
-                    cont2++;
+
+                    contr += 2;
                 }
 
-                if (cadeira[i][j].tipo != 1 && cadeira[i][j].tipo != 2)
+                if (t.cadeira[i][j].tipo != 1 && t.cadeira[i][j].tipo != 2)
                 {
+
                     do
                     {
                         printf("Tipo invalido, digite 1 pra Meia e 2 para Inteira: ");
-                        scanf("%d", &cadeira[i][j].tipo);
-                    } while (cadeira[i][j].tipo != 1 && cadeira[i][j].tipo != 2);
+                        scanf("%d", &t.cadeira[i][j].tipo);
+                    } while (t.cadeira[i][j].tipo != 1 && t.cadeira[i][j].tipo != 2);
                 }
 
-                strcpy(cadeira[i][j].status, "R");
+                t.cadeira[i][j].status = 'R';
                 printf("\nCadeira reservada com sucesso!\n");
             }
             else
@@ -356,7 +494,7 @@ int main()
             }
             break;
 
-        case 4:
+        case 4: // Quarta opção, consultar disponibilidade de uma cadeira específica.
 
             printf("Digite fileira da cadeira que deseja consultar: ");
             scanf(" %c", &aux);
@@ -366,32 +504,97 @@ int main()
             getchar();
             j--;
 
-            if (cadeira[i][j].status[0] == 'L')
+            if (t.cadeira[i][j].status == 'L')
             {
-                printf("A cadeira está livre\n");
-            }
-            else if (cadeira[i][j].status[0] == 'O')
-            {
-                printf("\nA cadeira está ocupada por: ");
-                imprimirDados(i, j, cadeira);
-            }
-            else if (cadeira[i][j].status[0] == 'R')
-            {
-                printf("A cadeira está reservada por: ");
-                imprimirDados(i, j, cadeira);
-            }
 
+                printf("A cadeira esta livre\n");
+            }
+            else if (t.cadeira[i][j].status == 'O')
+            {
+
+                printf("\nA cadeira esta ocupada por: ");
+                imprimirDados(i, j, t);
+            }
+            else if (t.cadeira[i][j].status == 'R')
+            {
+
+                printf("A cadeira esta reservada por: ");
+                imprimirDados(i, j, t);
+            }
             break;
 
-        case 5:
-            printf("Total arrecadado: R$ %d,00 \n", (cont * 15) + (cont2 * 30));
+        case 5: // Quinta opção, arrecadação e valores de reservas.
+
+            system("clear");
+            printf("Total reservado: R$ %d,00 \n", (contr * 15));
+            printf("Total arrecadado: R$ %d,00 \n", (cont * 15));
+            break;
+
+        case 6: // Sexta opção, cancelamento de todas as reservas.
+
+            system("clear");
+            for (i = 0; i < 22; i++)
+                for (j = 0; j < 30; j++)
+                    if (t.cadeira[i][j].status == 'R')
+                    {
+                        t.cadeira[i][j].status = 'L';
+                    }
+            printf("\nTODAS AS RESERVAS FORAM CANCELADAS\n");
+            break;
+
+        case 7: // Sétima opção, cancelamento de uma cadeira específica.
+
+            printf("Digite fileira da cadeira que deseja desocupar: ");
+            scanf(" %c", &aux);
+            numeroletra(aux, &i);
+            printf("Digite coluna da cadeira que deseja desocupar:  ");
+            scanf("%d", &j);
+            getchar();
+            j--;
+            t.cadeira[i][j].status = 'L';
+            printf("\nCadeira desocupada com sucesso!\n");
+            break;
+
+        case 8:
+            k = 0;
+            for (i = 0; i < 22; i++)
+                for (j = 0; j < 30; j++)
+                    if (t.cadeira[i][j].status == 'R')
+                    {
+                        r.ult++;
+                        strcpy(r.rel[k], ". ");
+                        strcat(r.rel[k], t.cadeira[i][j].nome);
+                        strcat(r.rel[k], " - tel:");
+                        strcat(r.rel[k], t.cadeira[i][j].tel);
+
+                        k++;
+                    }
+            if (k == 0)
+            {
+
+                strcpy(r.rel[k], "Nao foi encontrada nenhuma reserva!");
+                r.ult++;
+            }
+
+            relatorio(r);
+
+            printf("Relatório gerado com sucesso!\n");
+            break;
+
+        case 9: // Oitava opção, finalização do sistema.
+
+            system("clear");
+            printf("OBRIGADO POR UTILIZAR O NOSSO SISTEMA!\n");
+            criar_arq(t);
+            break;
+
+        default: // Controle de opções caso o usuário digite valores diferentes do intervado de [1-8]
+            printf("Digite uma opcao valida:\n");
             break;
         }
 
         printf("\n");
-    } while (op != 6);
-
-    printf("OBRIGADO POR UTILIZAR O NOSSO SISTEMA!");
+    } while (op != 9); // Ciclo para repetir o menu até a finalização dele com a operação (8).
 
     return 0;
 }
